7145974b35a9c24bc4ed6ce15f1df30d594eef4a
Jakob <jakob@jeykey.net>
2023-05-04 16:47:01 +0200

    Very basic working reply mechanics.

--- src/runtime/channel/direct.rs
+++ src/runtime/channel/direct.rs
@@ -7,7 +7,7 @@ use crossbeam_channel::{unbounded, Receiver, Sender};
 pub struct Connector<State, T> {
     sender: Sender<Event<T>>,
     receiver: Receiver<Event<T>>,
-    executor: Executor<State, T>,
+    pub executor: Executor<State, T>,
     counter: usize,
 }
 
@@ -38,7 +38,7 @@ impl<S: Sync, T: Send + Sync + 'static> Connector<S, T> {
 
     pub fn call<F>(&mut self, message: T, f: F) -> anyhow::Result<()>
     where
-        F: Fn(&S, &T) -> anyhow::Result<usize> + 'static,
+        F: FnMut(&mut S, T) -> anyhow::Result<usize> + 'static,
     {
         let id = self.counter;
         self.counter += 1;
@@ -47,10 +47,59 @@ impl<S: Sync, T: Send + Sync + 'static> Connector<S, T> {
         Ok(())
     }
 
-    pub fn receive(&mut self) -> anyhow::Result<Event<T>> {
-        let Ok(msg) = self.receiver.recv() else {
+    pub fn reply(&mut self, id: usize, message: T) -> anyhow::Result<()> {
+        let _ = self.sender.send(Event::Reply(id, message))?;
+        Ok(())
+    }
+
+    pub fn receive(&mut self) -> anyhow::Result<(usize, T)> {
+        let Ok(msg) = self.receiver.try_recv() else {
             return Err(anyhow::Error::new(Error::default()));
         };
-        Ok(msg)
+
+        match msg {
+            Event::Message(u, m) => Ok((u, m)),
+            Event::Reply(u, m) => {
+                self.executor.handle(u, m)?;
+                self.receive() 
+            }
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[derive(Debug)]
+    enum Message {
+        Request,
+        Send(usize)
+    }
+
+    #[test]
+    fn test_basic_message_exchange() {
+        let (mut c1, mut c2) = Connector::new(0, 0);
+
+        let _ = c1.call(Message::Request, |ref mut state, message| {
+            println!("c1 received: {:?}", message);
+            match message {
+                Message::Request => {},
+                Message::Send(i) => **state = i
+            }
+            Ok(0)
+        });
+
+        if let Ok((c, msg)) = c2.receive() {
+            println!("c2 received: {:?}", msg);
+            let _ = c2.reply(c, Message::Send(42));
+        }
+
+        let _ = c1.receive();
+
+        let t = std::time::Duration::from_millis(100);
+        std::thread::sleep(t);
+
+        assert_eq!(c1.executor.state, 42);
     }
 }
--- src/runtime/channel/executor.rs
+++ src/runtime/channel/executor.rs
@@ -4,11 +4,11 @@ use crate::Error;
 use std::collections::HashMap;
 
 pub struct Executor<State, R> {
-    state: State,
-    active: HashMap<usize, (Box<dyn Fn(&State, &R) -> anyhow::Result<usize>>, usize)>,
+    pub state: State,
+    active: HashMap<usize, (Box<dyn FnMut(&mut State, R) -> anyhow::Result<usize>>, usize)>,
 }
 
-impl<S: Sync, R> Executor<S, R> {
+impl<S, R> Executor<S, R> {
     pub fn new(state: S) -> Self {
         Self {
             active: HashMap::new(),
@@ -18,14 +18,14 @@ impl<S: Sync, R> Executor<S, R> {
 
     pub fn add<F>(&mut self, f: F, mid: usize, c: usize)
     where
-        F: Fn(&S, &R) -> anyhow::Result<usize> + 'static,
+        F: FnMut(&mut S, R) -> anyhow::Result<usize> + 'static,
     {
         self.active.insert(mid, (Box::new(f), c));
     }
 
-    pub fn handle(&mut self, id: usize, message: &R) -> Result<(), Error> {
+    pub fn handle(&mut self, id: usize, message: R) -> Result<(), Error> {
         if let Some((f, c)) = self.active.get_mut(&id) {
-            match (f)(&self.state, message) {
+            match (f)(&mut self.state, message) {
                 Ok(count) => {
                     *c = count;
                     return Ok(());
